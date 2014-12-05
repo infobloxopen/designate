05349e57259603a1354724414475a19a0caeaa26
Nikolay Yakovlev <nyakovlev@mirantis.com>
2014-12-05 12:31:47 +0200

    * Copyright fixes * DB opts removed * Docstring fix

--- designate/backend/impl_infoblox/record_factory.py
+++ designate/backend/impl_infoblox/record_factory.py
@@ -1,6 +1,4 @@
-# Copyright 2014 Mirantis
-#
-# Author: Nikolay Yakovlev <nyakovlev@mirantis.com>
+# Copyright 2014 Infoblox
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
